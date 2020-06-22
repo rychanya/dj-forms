a5a71c6862690091874d79680563d46c66251635
Dmitrii Rumyancev <rychanya@gmail.com>
2020-06-23 02:42:16 +0300

    task 2

--- review/app/views.py
+++ review/app/views.py
@@ -19,15 +19,26 @@ def product_list_view(request):
 def product_view(request, pk):
     template = 'app/product_detail.html'
     product = get_object_or_404(Product, id=pk)
-
-    form = ReviewForm
-    if request.method == 'POST':
-        # логика для добавления отзыва
-        pass
+    reviews = Review.objects.filter(product=product)
+    is_review_exist = product.id in request.session.get('reviewed_products', [])
+    
+    if request.method == 'POST' and not is_review_exist:
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.product = product
+            review.save()
+            reviewd_list = request.session.get('reviewed_products', [])
+            reviewd_list.append(product.id)
+            request.session['reviewed_products'] = reviewd_list
+            is_review_exist = True
+    form = ReviewForm()
 
     context = {
         'form': form,
-        'product': product
+        'product': product,
+        'is_review_exist': is_review_exist,
+        'reviews': reviews
     }
 
     return render(request, template, context)
